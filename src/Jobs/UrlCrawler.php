5106c2dea0b2bccde431af7aa2ad7533459fd5c5
Lukas Alexander Kienzl <a1409215@unet.univie.ac.at>
2018-04-28 00:57:48 +0200

    fix bug

--- src/Jobs/UrlCrawler.php
+++ src/Jobs/UrlCrawler.php
@@ -86,7 +86,7 @@ class UrlCrawler implements ShouldQueue
 
             $this->browseToWebsite($source['url']);
 
-            $urls[] = $this->getUrlsFromCurrentWebsite($source['options']);
+            array_push($urls, $this->getUrlsFromCurrentWebsite($source['options']));
 
             if (!$this->pagingEnabled()) {
                 continue;
@@ -95,7 +95,7 @@ class UrlCrawler implements ShouldQueue
             while ($this->getPagingElement()->count()) {
                 $nextUrl = $this->getPagingElement()->first()->attr('href');
                 $this->browseToWebsite(Helper::generateAbsoluteUrl($nextUrl, $source['url']));
-                $this->getUrlsFromCurrentWebsite($source['options']);
+                array_push($urls, $this->getUrlsFromCurrentWebsite($source['options']));
             }
         }
 
@@ -124,7 +124,7 @@ class UrlCrawler implements ShouldQueue
 
         $this->currentWebsite->filter($this->cssSelectors['detailPageLink'])->each(function (Crawler $link) use ($options, $absoluteUrl, &$urls) {
             $url = Helper::generateAbsoluteUrl($link->attr('href'), $absoluteUrl);
-            $urls[] = compact('url', 'options');
+            array_push($urls, compact('url', 'options'));
         });
 
         return $urls;
